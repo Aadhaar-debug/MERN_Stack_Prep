8d640de53188ed0efdd9bca7d26ad9ba32a42d49
aadhaar-debug <aadhaarkoul2002@gmail.com>
2024-09-25 09:29:17 +0530

    Start

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-primary'>
+        <h1 className='text-start mt-5'>Welcome to Bootstrap</h1>
+        <h1 className='text-center mb-5 mt-5'>Welcome to Bootstrap</h1>
+        <h1 className='text-end'>Welcome to Bootstrap</h1>
+        <h1 className=''>Welcome to Bootstrap</h1>
+        <h1 className=''>Welcome to Bootstrap</h1>
+        <h1 className=''>Welcome to Bootstrap</h1>
+        <h1 className=''>Welcome to Bootstrap</h1>
+        <h1 className=''>Welcome to Bootstrap</h1>
+        <h1 className=''>Welcome to Bootstrap</h1>
+        <h1 className=''>Welcome to Bootstrap</h1>
+        <h1 className=''>Welcome to Bootstrap</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
